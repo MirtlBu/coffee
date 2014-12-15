a8ea6ab93d2c0fe0780310956dbbfdfbeaf8115d
mirtlbu <bubuzoglo@gmail.com>
2014-12-15 16:48:54 +0300

    finished sorts switcher

--- js/template.js
+++ js/template.js
@@ -21,7 +21,119 @@ var machines = [
 			"Одновременная раздача на 2 чашки"
 		]
 	},
-]
+];
+
+var sorts = [
+	{	
+		country: "New",
+		url: "img/sort-icon-1.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 2000},
+				{sort: "Мексика Маргонджип", price: 2600},
+				{sort: "Карамель Тоффи", price: 2300}
+			],
+		_id: 'sort_1'	
+	},
+	{	
+		country: "America",
+		url: "img/sort-icon-2.png",
+		items:
+			[
+				{sort: "Мексика Маргонджип", price: 2600},
+				{sort: "Карамель Тоффи", price: 2300},
+				{sort: "Сабра (шоколад, апельсин)", price: 2500}
+			],
+		_id: 'sort_2'
+	},
+	{
+		country: "Africa",
+		url: "img/sort-icon-3.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 2000},
+				{sort: "Мексика Маргонджип", price: 2600}
+				
+			],
+		_id: 'sort_3'
+	},
+	{
+		country: "Indonesia",
+		url: "img/sort-icon-4.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 2000},
+				{sort: "Мексика Маргонджип", price: 2600},
+				{sort: "Карамель Тоффи", price: 2300},
+				{sort: "Сабра (шоколад, апельсин)", price: 2500}
+			],
+		_id: 'sort_4'
+	},
+	{
+		country: "Exotic",
+		url: "img/sort-icon-5.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 5000}
+			],
+		_id: 'sort_5'
+	},
+	{
+		country: "Elit",
+		url: "img/sort-icon-6.png",
+		items:
+			[
+				{sort: "Мексика Маргонджип", price: 2600},
+				{sort: "Карамель Тоффи", price: 2400},
+				{sort: "Сабра (шоколад, апельсин)", price: 2000}
+			],
+		_id: 'sort_6'
+	},
+	{
+		country: "Espresso",
+		url: "img/sort-icon-8.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 2700},
+				{sort: "Мексика Маргонджип", price: 2300},
+				{sort: "Карамель Тоффи", price: 2300}
+			],
+		_id: 'sort_7'
+	},
+	{
+		country: "Robusta",
+		url: "img/sort-icon-8.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 1000},
+				{sort: "Мексика Маргонджип", price: 3600}
+			],
+		_id: 'sort_8'
+	},
+	{
+		country: "Aromatized",
+		url: "img/sort-icon-9.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 2400},
+				{sort: "Сабра (шоколад, апельсин)", price: 2500}
+			],
+		_id: 'sort_9'
+	},
+	{
+		country: "Green coffee",
+		url: "img/sort-icon-10.png",
+		items:
+			[
+				{sort: "Куба Туркино Лавадо", price: 2000},
+				{sort: "Мексика Маргонджип", price: 2600},
+				{sort: "Карамель Тоффи", price: 2300},
+				{sort: "Сабра (шоколад, апельсин)", price: 2500}
+			],
+		_id: 'sort_10'
+	},
+	
+];
 
 function renderMachine(machine) {
 	return $('<div/>', {'class': 'slider__item'})
@@ -43,4 +155,23 @@ function renderMachine(machine) {
 				.append($('<span>От 2,5 кг в месяц</span>')))
 			.append($('<button/>', {'class': 'button button--placeorder'}).text('Заказать')));
 }
+
+function renderSorts(sort) {
+	return $('<div/>', {'class': 'icon__item', 'id': sort._id})
+		.append($('<img/>', {src: sort.url, width: 61, height: 60}))
+		.append($('<div>', {'class': 'icon__text'}).text(sort.country))
+}
+
+function renderList(data) {
+	return $('<li/>')
+		.append($('<div/>', {'class': 'sort'}).text(data.sort))
+		.append($('<div/>', {'class': 'price'})
+			.append($('<span/>').text(data.price))
+			.append($('<span/>').text(' кг')))
+
+			
+}
+
+
+
 
